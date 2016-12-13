911387aa92779b7040a91f1aa55c0f9d24729614
wintvelt <wintvelt@me.com>
2016-12-13 23:01:58 +0100

    styles and package install

---
+++ src/Styles.elm
@@ -0,0 +1,15 @@
+module Styles exposing (..)
+{- Styles to use in examples
+-}
+
+maindiv = 
+    [ ("padding", "16px")
+    , ("display","flex")
+    ]
+
+celldiv =
+    [ ("padding","8px")
+    , ("width","32px")
+    , ("height","32px")
+    , ("border", "1px solid grey")
+    ]
