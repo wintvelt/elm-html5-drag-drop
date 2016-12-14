48b93e4c1037c230f19accc5ec9457fabab89bf1
wintvelt <wintvelt@me.com>
2016-12-14 22:05:52 +0100

    more drag events scaffolding

--- src/DragEvents.elm
+++ src/DragEvents.elm
@@ -2,30 +2,32 @@ module DragEvents exposing (..)
 {- custom events for Html5 drag and drop
 -}
 import Html.Events exposing (on, onWithOptions)
+import Html exposing (Attribute)
+import Json.Decode as JD
 
 -- when drag of element starts
-onDragStart : 
+onDragStart : msg -> Attribute msg
 
 -- when element is being dragged (fires multiple times between dragStart and dragEnd)
-onDrag : 
+onDrag : msg -> Attribute msg
 
 -- when this element is no longer being dragged
-onDragEnd :
+onDragEnd : msg -> Attribute msg
 
 
 -- when another dragged element enters this element
-onDragEnter :
+onDragEnter : msg -> Attribute msg
 
 
 -- when another dragged element is over this element (fired multiple times during drag)
-onDragOver :
+onDragOver : msg -> Attribute msg
 
 
 -- when another dragged element leaves this element (moved outside border)
-onDragLeave :
+onDragLeave : msg -> Attribute msg
 
 
 -- when another dragged element is dropped over this element
-onDrop : 
+onDrop : msg -> Attribute msg
 
 
